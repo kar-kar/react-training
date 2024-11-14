11b4936d27865f4eff650beae2efa292e5b98264
kar-kar <karzhannikov@gmail.com>
2024-11-14 17:15:34 +0100

    fix movie details rendering

--- src/MovieDetailsView.tsx
+++ src/MovieDetailsView.tsx
@@ -13,10 +13,10 @@ export default function MovieDetailsView(props: MovieDetailsViewProps) {
                     <p>{props.movie.overview}</p>
                     <p>{props.movie.genres.map(genre => (<span className="genre-badge badge text-bg-primary">{genre.name}</span>))}</p>
                     <p>Release Date: {props.movie.release_date}</p>
-                    {props.movie.budget &&
-                        <p>Budget: ${props.movie.budget.toLocaleString()}</p>}
-                    {props.movie.revenue &&
-                        <p>Revenue: ${props.movie.revenue.toLocaleString()}</p>}
+                    {(props.movie.budget ?? 0) > 0 &&
+                        <p>Budget: ${props.movie.budget!.toLocaleString()}</p>}
+                    {(props.movie.revenue ?? 0) > 0 &&
+                        <p>Revenue: ${props.movie.revenue!.toLocaleString()}</p>}
                 </div>
                 <img className="movie-details-poster"
                     src={`http://image.tmdb.org/t/p/w500${props.movie.poster_path}`}
